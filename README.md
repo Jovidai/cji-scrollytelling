794d76270e641b8cf4187e58298a5b733bb1cc6d
Jovidai <daizhaozhou1999@gmail.com>
2023-09-17 17:50:45 -0700

    update all

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# cji-scrollytelling
+# New England Innocence Project - Paul Robinson
+
+This repository contains the story and code for a scrolly-telling story designed for the New England Innocence Project.  --- Joli Holmes
+
+## Git command line cheat sheet  --- Joli Holmes
+
+* `git add (name of file)`
+* `git commit -m "commit message"`
+* `git push` - pushes changes to the server
+
+* `git pull` - pulls changes from the server
+* `git status` - tells you which files were changes
+* `git diff` - tells you the changes that were made to each file
+
+* `git checkout` lets you navigate between branches
+* `git checkout -b 'new-name'` - creates a new branch from the branch that you're currently on/open
+* `git branch` - lists all of your git branches
---
+++ js/project.js
@@ -0,0 +1,104 @@
+// using d3 for convenience, and storing a selected elements
+var container = d3.select('#scroll');
+var graphic = container.select('.scroll__graphic');
+var text = container.select('.scroll__text');
+var step = text.selectAll('.step');
+var media = document.getElementById('media');
+var caption = document.getElementById('caption');
+
+// initialize the scrollama
+var scroller = scrollama();
+
+// resize function to set dimensions on load and on page resize
+function handleResize() {
+    scroller.resize();
+}
+
+// scrollama event handlers
+function handleStepEnter(response) {
+
+    // sticky the graphic
+    if (response.index === 0) {
+        graphic.classed('is-fixed', true);
+    }
+
+    // fade in current step
+    step.classed('is-active', function (d, i) {
+        return i === response.index;
+    })
+
+    const backdrops = [
+        { 'src': './assets/images/CJI-Population-1.jpg',
+        //   'caption': 'A front view of the family home where Robinson lives with his nieces and nephews.',
+        //   'alt': 'A front view of the two-story family home where Robinson lives. The house is white with horizontal siding. Red shutters and a red deck give the house a nice accent color.'
+        },
+        { 'src': './assets/images/CJI-Population-2.jpg',
+        //  'caption': 'Family picture wall at the Robinson house of three to four generations of Robinsons. The central photo is Paul\'s niece, left, grand-niece, center, and sister, right. The other photos on the wall are his nephews and nieces.',
+        //  'alt': 'Family picture wall at the Robinson house of three to four generations of Robinsons. The central photo is Paul\'s niece, left, grand-niece, center, and sister, right. The other photos on the wall are his nephews and nieces.'
+        },
+        { 'src': './assets/images/CJI-Population-3.jpg',
+        //   'caption': 'Paul in his silver Mini Cooper.',
+        //   'alt': 'Paul in his silver Mini Cooper. His hand is on his knee and he is turned towards the camera with a partial smile.'
+        },
+        { 'src': './assets/images/CJI-Population-4.jpg',
+        //   'caption': 'A side-view of the Robinson family home.',
+        //   'alt': 'A side view of the Robinson family home. A BBQ can be seen at the end of the porch..'
+        },
+        { 'src': './assets/images/CJI-Population-5.jpg',
+        // 'caption': 'A side-view of the Robinson family home.',
+        // 'alt': 'A side view of the Robinson family home. A BBQ can be seen at the end of the porch..'
+        },
+        { 'src': './assets/images/CJI-Population-6.jpg',
+        // 'caption': 'A side-view of the Robinson family home.',
+        // 'alt': 'A side view of the Robinson family home. A BBQ can be seen at the end of the porch..'
+        },
+    ]
+    media.src = backdrops[response.index].src;
+    media.alt = backdrops[response.index].alt;
+    caption.textContent = backdrops[response.index].caption;
+}
+
+function handleStepExit(response) {
+    // if (response.index === 0) {
+    //     graphic.classed('is-fixed', false);
+    // }
+
+    window.onscroll = function() {
+        if ((window.innerHeight + Math.ceil(window.pageYOffset)) >= document.body.offsetHeight) {
+            graphic.classed('is-fixed', false);
+        }
+      }
+}
+
+// optional to view precise percent progress on callback
+function handleProgress(response) {
+    console.log(response)
+}
+
+// kick-off code to run once on load
+function init() {
+    // 1. call a resize on load to update width/height/position of elements
+    handleResize();
+
+    // 2. setup the scrollama instance
+    // 3. bind scrollama event handlers (this can be chained like below)
+    scroller
+        .setup({
+            container: '#scroll', // our outermost scrollytelling element
+            graphic: '.scroll__graphic', // the graphic
+            text: '.scroll__text', // the step container
+            step: '.scroll__text .step', // the step elements
+            offset: 0.5, // set the trigger to be 1/2 way down screen
+            // debug: true, // display the trigger offset for testing
+            progress: false
+        })
+        // .onStepProgress(handleProgress)
+        .onStepEnter(handleStepEnter)
+        .onStepExit(handleStepExit)
+
+    // setup resize event
+    window.addEventListener('resize', handleResize);
+}
+
+// start it up
+init();
